b79d02d0fd244e1837e65656f5418a1f62c471de
prashantmp <prashant.polshettiwar@gmail.com>
2015-11-27 18:40:43 +0530

    Exporting local project into GitHub

--- app/src/main/java/com/appliedsni/mygitapp/MainActivity.java
+++ app/src/main/java/com/appliedsni/mygitapp/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -48,5 +49,10 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void doWork(){
+        Log.w("MAX","Do some work here !!!");
+
     }
 }
